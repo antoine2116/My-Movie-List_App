42ef8ed3434cbeae33ec3859f5c0d6f5cbb53c07
antooinerobin <antooinerobin@gmail.com>
2023-03-02 19:54:19 +0100

    add outside click for user dropdown

--- components/navigation/UserDropdown.tsx
+++ components/navigation/UserDropdown.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { IoPersonCircle } from "react-icons/io5";
 import { removeUserToken } from "../../common/auth/UserToken";
 import { useAuth } from "../AuthContext";
+import OutsideClickHandler from "react-outside-click-handler";
 
 function NavbarUserButton() {
   const [displayDropdown, setDisplayDropdown] = useState(false);
@@ -23,21 +24,23 @@ function NavbarUserButton() {
       />
       { 
         displayDropdown && (
-          <div className="z-10 absolute right-0 border border-gray-100 bg-white divide-y divide-gray-100 rounded-lg shadow-md w-48">
-            <div className="px-4 py-3 text-sm text-gray-900">
-              <div>Signed in as</div>
-              <div className="font-medium">placeholder@gmail.com</div>
+          <OutsideClickHandler onOutsideClick={toggleDropdown}>
+            <div className="z-10 absolute right-0 border border-gray-100 bg-white divide-y divide-gray-100 rounded-lg shadow-md w-48">
+              <div className="px-4 py-3 text-sm text-gray-900">
+                <div>Signed in as</div>
+                <div className="font-medium">placeholder@gmail.com</div>
+              </div>
+              <ul>
+                <li className="px-4 py-3 hover:bg-gray-100 cursor-pointer">
+                  <a
+                    className="block text-sm text-gray-700"
+                    onClick={handleLogout}>
+                    Logout
+                  </a>
+                </li>
+              </ul>
             </div>
-            <ul>
-              <li className="px-4 py-3 hover:bg-gray-100 cursor-pointer">
-                <a 
-                  className="block text-sm text-gray-700"
-                  onClick={handleLogout}>
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </div>
+          </OutsideClickHandler>
         )
       }
     </div>
